b06362bd731aeb0c5db3fca659f74bf67741fa9e
Alex Bumpus <alexander.bumpus@yahoo.com>
2021-12-16 19:00:01 -0700

    Complete dev object challenge

---
+++ models/modelB.js
@@ -0,0 +1,11 @@
+var newDeveloper = {
+  devName: "Alex",
+  devPronouns: "he/him",
+  devAge: 23,
+  devCatsOwned: 2,
+  isDevHappy: true,
+  isDevHungry: true,
+  devCatNames: ["Ripley", "Bonnie"],
+  devFaveFoods: ["Pizza", "Chips", "Sour candy"]
+}
+// console.log(newDeveloper);
