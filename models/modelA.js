506348275cf08420fe18095ed4aac4bea14973fc
Alex Bumpus <alexander.bumpus@yahoo.com>
2021-12-16 18:53:18 -0700

    Complete object challenge

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var dino = {
+  dinoName: "Therizinosaurus",
+  dinoLocation: "Asia",
+  dinoAvgWeight: 11000,
+  dinoAvgHeight: 38,
+  isMeatEater: false,
+  isPlantEater: true,
+  dinoFoods: ["Leaves", "Plants", "Fruit"],
+  dinoDefenses: ["Large claws", "Height", "Teeth"]
+}
+// console.log(dino);
