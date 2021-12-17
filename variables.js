2f78ea120272988714c6878f7773f5bfb9658bd2
Alex Bumpus <alexander.bumpus@yahoo.com>
2021-12-16 18:36:19 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,10 @@
+var movie = "Avengers";
+var isEntertaining = true;
+movie = "A Quiet Place";
+var num4 = 4;
+var num6 = 6;
+product = num4 * num6;
+var favoriteFoods = ["Pizza", "Chips", "Sour Candy"];
+favoriteFoods.shift();
+// console.log(favoriteFoods);
+console.log(movie.length);
